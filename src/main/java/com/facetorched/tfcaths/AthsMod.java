1cf74064d20f5911a35e9c2dbed76df3e5a24eb0
facetorched <kktangent@gmail.com>
2022-01-20 03:28:07 -0600

    update version to 1.1.2

--- src/main/java/com/facetorched/tfcaths/AthsMod.java
+++ src/main/java/com/facetorched/tfcaths/AthsMod.java
@@ -19,7 +19,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class AthsMod
 {
     public static final String MODID = "tfcaths";
-    public static final String VERSION = "1.1.1"; // change in gradle.build as well
+    public static final String VERSION = "1.1.2"; // change in gradle.build as well
     public static final String NAME = "TFC+ Aesthetics";
     
     @SidedProxy(
